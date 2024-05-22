fe15363e93e0bf276694b5171df808d27f892e37
SanzMDG <dafingarnida01@gmail.com>
2024-05-22 10:36:37 +0700

    first commit

---
+++ Praktikum02/Submit.php
@@ -0,0 +1,20 @@
+<?php
+$nama = $_POST['Nama'];
+$matkul = $_POST['Matkul'];
+$tugas = $_POST['Tugas'];
+$uts = $_POST['uts'];
+$uas = $_POST['uas'];
+
+echo "Nama Lengkap : " . $nama;
+echo "<br>";
+echo "Mata Kuliah : " . $matkul;
+echo "<br>";
+echo "Nilai Tugas : " . $tugas;
+echo "<br>";
+echo "Nilai UTS : " . $uts;
+echo "<br>";
+echo "Nilai UAS : " . $uas;
+
+
+
+?>
---
+++ README.md
@@ -0,0 +1,3 @@
+# praktikum-web-2
+# project_1
+# project_1
